35750a9344bb0fab06509c954dd73abce286f76c
yyang325 <frank.young325@gmail.com>
2016-05-02 15:20:56 -0400

    service example in angularjs.org developer document

---
+++ conceptualOverview/plunk/invoice3.js
@@ -0,0 +1,23 @@
+(function(angular) {
+  'use strict';
+angular.module('invoice3', ['finance3'])
+  .controller('InvoiceController', ['currencyConverter', function(currencyConverter) {
+    this.qty = 1;
+    this.cost = 2;
+    this.inCurr = 'EUR';
+    this.currencies = currencyConverter.currencies;
+
+    this.total = function total(outCurr) {
+      return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
+    };
+    this.pay = function pay() {
+      window.alert("Thanks!");
+    };
+  }]);
+})(window.angular);
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
